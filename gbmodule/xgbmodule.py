04d4c4875771c85bf7c6db468237d08e3a9a3138
mthorrell <mthorrell@github.com>
2024-05-05 15:11:26 -0500

    added xgb

---
+++ gbmodule/xgbmodule.py
@@ -0,0 +1,111 @@
+from typing import Tuple
+
+import numpy as np
+import torch
+from torch import nn
+import xgboost as xgb
+
+
+class XGBModule(nn.Module):
+    def __init__(
+        self,
+        batch_size,
+        input_dim,
+        output_dim,
+        params={},
+    ):
+        super(XGBModule, self).__init__()
+        self.batch_size = batch_size
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+        self.params = params
+        self.params["objective"] = "reg:squarederror"
+        self.params["base_score"] = 0
+
+        init_matrix = np.zeros([batch_size, input_dim])
+        self.bst = xgb.train(
+            self.params,
+            xgb.DMatrix(init_matrix, label=np.zeros(batch_size * output_dim)),
+            num_boost_round=0,
+        )
+        self.n_completed_boost_rounds = 0
+        self.dtrain = None
+
+        self.FX = nn.Parameter(
+            torch.tensor(
+                np.zeros([batch_size, output_dim]),
+                dtype=torch.float,
+            )
+        )
+
+    def forward(self, input_array, return_tensor=True):
+        # TODO relate this to training mode and, ultimately actual
+        # batch training
+        if self.dtrain is not None:
+            preds = self.bst.predict(self.dtrain)
+        else:
+            preds = self.bst.predict(xgb.DMatrix(input_array))
+
+        FX_detach = self.FX.detach()
+        FX_detach.copy_(
+            torch.tensor(
+                preds.reshape([self.batch_size, self.output_dim]), dtype=torch.float
+            )
+        )
+
+        if return_tensor:
+            return self.FX
+        return preds
+
+    def gb_step(self, input_array):
+        grad = self.FX.grad * self.batch_size
+
+        # parameters are independent row by row, so we can
+        # at least calculate hessians column by column by
+        # considering the sum of the gradient columns
+        hesses = []
+        for i in range(self.output_dim):
+            hesses.append(
+                torch.autograd.grad(grad[:, i].sum(), self.FX, retain_graph=True)[0][
+                    :, i : (i + 1)
+                ]
+            )
+        hess = torch.cat(hesses, axis=1)
+
+        obj = XGBObj(grad, hess)
+        if self.dtrain is None:
+            self.dtrain = xgb.DMatrix(
+                input_array, label=np.zeros(self.batch_size * self.output_dim)
+            )
+
+        g, h = obj(np.zeros([self.batch_size, self.output_dim]), None)
+
+        self.bst.boost(
+            self.dtrain,
+            # self.n_completed_boost_rounds+1,
+            g,
+            h,
+        )
+
+        self.n_completed_boost_rounds = self.n_completed_boost_rounds + 1
+
+
+class XGBObj:
+    def __init__(self, grad, hess):
+        self.grad = grad
+        self.hess = hess
+
+    def __call__(self, preds, dtrain):
+
+        if len(preds.shape) == 2:
+            M = preds.shape[0]
+            N = preds.shape[1]
+        else:
+            M = preds.shape[0]
+            N = 1
+
+        g = self.grad.detach().numpy().reshape([M * N, 1])
+        h = self.hess.detach().numpy().reshape([M * N, 1])
+
+        return g, h
