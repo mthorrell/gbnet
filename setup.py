31f617feae7ecefa19825147bef947ddd787a57b
mthorrell <mthorrell@github.com>
2024-09-15 15:12:21 -0500

    updated version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gboost_module",
-    version="0.1.2",
+    version="0.1.4",
     author="Michael Horrell",
     author_email="mthorrell@github.com",
     description="Torch modules using popular boosting libraries",
