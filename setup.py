06a588927ac84379342fd1a33b805e7f469457c1
mthorrell <horrellm87@gmail.com>
2025-10-21 21:26:23 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="gbnet",
-    version="0.7.1",
+    version="0.7.2",
     author="Michael Horrell",
     author_email="mthorrell@github.com",
     description="Gradient Boosting libraries integrated with PyTorch",
